b09507b06c4d8d625a096946206ba360a2ae094c
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-30 22:39:57 -0500

    Done.

--- src/Routes.js
+++ src/Routes.js
@@ -9,7 +9,10 @@ import Movies from './components/Movies'
 
 const Routes = (
     <Route path="/" component={App}>
-        {/* routes go here */}
+      <IndexRoute component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/actors" component={Actors} />
+        <Route path="/directors" component={Directors} />
     </Route>
 )
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,14 @@ import { Link } from 'react-router'
 
 const NavBar = () => {
     return (
-        <div>
-            {/* code goes here */}
-        </div>
+      <div>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/movies'>Movies</Link></li>
+          <li><Link to='/directors'>Directors</Link></li>
+          <li><Link to='/actors'>Actors</Link></li>
+        </ul>
+      </div>
     )
 
 }
